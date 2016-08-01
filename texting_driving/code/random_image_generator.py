175e652bfad1ef16b4d8aeb8c257127580b9d8ea
Raj Agrawal <r.agrawal@berkeley.edu>
2016-08-01 14:35:28 -0500

    added documentation and 2d image generator

--- texting_driving/code/random_image_generator.py
+++ texting_driving/code/random_image_generator.py
@@ -1,13 +1,28 @@
+# Author: Raj Agrawal 
 
-# Taken mostly from https://jessesw.com/Deep-Learning/
+# Helper functions to randomly rotate multiple frames (or just 2d images) 
+# in the minibatch samples  
+
+# References: Taken mostly from https://jessesw.com/Deep-Learning/
 
 from scipy.ndimage import convolve, rotate
 import numpy as np
 
 def random_image_generator(image_stack):
     """
-    This function will randomly translate and rotate multiple image frames 
-    (all in same direction), producing a new, altered version as output.
+    Overview: 
+        This function randomly translates and rotates multiple 
+        image frames (all in same direction), producing a new, 
+        altered version as output. 
+    ----------
+    image_stack: numpy array    
+        A numpy array of shape (1, num_frames, length, width) 
+
+    Returns
+    -------
+    new_image: numpy array  
+        A numpy array of shape (1, num_frames, length, width) 
+        that is the randomly rotated version of 'image_stack'
     """
     num_frames = image_stack.shape[1]
     length = image_stack.shape[2]
@@ -49,4 +64,61 @@ def random_image_generator(image_stack):
         # Rotate the image
         rotated = rotate(moved, angle, reshape = False)
         new_image[0, i, :, :] = rotated
+    return new_image
+
+def random_2D_image_generator(image):
+    """
+    Overview: 
+        This function randomly translates and rotates multiple 
+        image frames (all in same direction), producing a new, 
+        altered version as output. 
+    ----------
+    image_stack: numpy array    
+        A numpy array of shape (1, length, width) 
+
+    Returns
+    -------
+    new_image: numpy array  
+        A numpy array of shape (1, num_frames, length, width) 
+        that is the randomly rotated version of 'image_stack'
+    Note:
+        Warning: NOT TESTED but should be right. 
+    """
+    length = image_stack.shape[1]
+    width = image_stack.shape[2]
+    # Create our movement vectors for translation first. 
+        
+    move_up = [[0, 1, 0],
+               [0, 0, 0],
+               [0, 0, 0]]
+        
+    move_left = [[0, 0, 0],
+                 [1, 0, 0],
+                 [0, 0, 0]]
+        
+    move_right = [[0, 0, 0],
+                  [0, 0, 1],
+                  [0, 0, 0]]
+                                   
+    move_down = [[0, 0, 0],
+                 [0, 0, 0],
+                 [0, 1, 0]]
+        
+    # Create a dict to store these directions in.
+        
+    dir_dict = {1:move_up, 2:move_left, 3:move_right, 4:move_down}
+        
+    # Pick a random direction to move.
+        
+    direction = dir_dict[np.random.randint(1,5)]
+        
+    # Pick a random angle to rotate (30 degrees clockwise to 30 degrees counter-clockwise).
+        
+    angle = np.random.randint(-30,31)
+        
+    # Move the random direction and change the pixel data back to a 2D shape.
+    moved = convolve(image.reshape(length,width), direction, mode = 'constant')
+
+    # Rotate the image
+    new_image = rotate(moved, angle, reshape = False)
     return new_image
