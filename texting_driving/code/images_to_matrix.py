b342cf173519cc50960eec14f00fa411ec501a18
Raj Agrawal <r.agrawal@berkeley.edu>
2016-08-01 14:55:15 -0500

    added more documentation

--- texting_driving/code/images_to_matrix.py
+++ texting_driving/code/images_to_matrix.py
@@ -6,8 +6,6 @@
 # Run $ffmpeg -i NAME.MOV -r 20 ../data/train/images/image_sequence%d.jpeg from
 # code folder to generate images in the training images folder
 
-# Add more documentation, convert to pep8 
-
 from __future__ import division 
 
 import os
@@ -20,15 +18,88 @@ from scipy.ndimage.interpolation import zoom
 
 def readImage(path, reduction_factor=.15):
     """
-    - Reduces photo size by a factor of 1/reduction_factor
-    - Converts to greyscale 
+    Overview: 
+        Reduces photo size by a factor of 1/reduction_factor 
+        and converts to greyscale  
+    ----------
+    path: string   
+        Path where the image is is located. 
+    
+    reduction_factor: float
+        Defaults to .15. reduction_factor > 1 enlarges the photo and 
+        reduction_factor < 1 shrinks the photo. 
+
+    Returns
+    -------
+    reduced_image: numpy array 
+        Grayscaled and resized image   
     """
     grey_image = imread(path, flatten=True)
     return zoom(grey_image, reduction_factor)
 
-def toMatrix(paths, num_frames, imgsize=(81, 144)):
+def to3DMatrix(paths, imgsize=(81, 144), reduction_factor=.15):
     """
-    - num_samples x num_frames x length x width array 
+    Overview: 
+        Reduces images size by a factor of 1/reduction_factor 
+        and converts to greyscale and stores in an array of
+        shape (len(paths), length, width).  
+    ----------
+    paths: list   
+        List consisting of paths where the images are located. See  
+
+    imgsize: tuple
+        The (length,width) of image and defaults to (81, 144). 
+    
+    reduction_factor: float
+        Defaults to .15. reduction_factor > 1 enlarges the photo and 
+        reduction_factor < 1 shrinks the photo. 
+
+    Returns
+    -------
+    images_by_time: numpy array 
+        Grayscaled and resized images of shape 
+        (len(paths), length, width) 
+    """
+    num_images = len(paths)
+    images_by_time = np.zeros(shape=(num_images, imgsize[0], imgsize[1])) 
+    for i, path in enumerate(paths):
+        image = readImage(path, reduction_factor)
+        images_by_time[i, :, :] = image
+        print('Finished Processing image ' + str(i))
+    return images_by_time
+
+def toMatrix(paths, num_frames, imgsize=(81, 144), reduction_factor=.15):
+    """
+    Overview: 
+        Reduces images size by a factor of 1/reduction_factor 
+        and converts to greyscale. Each 'sample' is of shape 
+        num_frames x imgsize. This aggregates all samples into 
+        a numpy array. 
+    ----------
+    paths: list   
+        List consisting of paths where the images are located. See 
+        ** Note ** below 
+
+    num_frames: int 
+        The number of images in one sample
+
+    imgsize: tuple
+        The (length,width) of image and defaults to (81, 144). 
+    
+    reduction_factor: float
+        Defaults to .15. reduction_factor > 1 enlarges the photo and 
+        reduction_factor < 1 shrinks the photo. 
+
+    Returns
+    -------
+    images_by_time: numpy array 
+        Grayscaled and resized images of shape 
+        (num_samples, num_frames, length, width)
+
+    Note: 
+        This ASSUMES that the paths are in the right order. In other words, 
+        if paths = [im1, im2, im3, im4] and num_frames = 2 this means sample
+        1 is [im1, im2] and sample 2 is [im3, im4].   
     """
     num_images = len(paths)
     num_samples = int(num_images / num_frames)
@@ -36,21 +107,9 @@ def toMatrix(paths, num_frames, imgsize=(81, 144)):
     for sample_index in range(num_samples):
         index_in_array = sample_index * num_frames
         sample_paths = paths[index_in_array:(num_frames + index_in_array)]
-        sample = to3DMatrix(sample_paths, imgsize)
+        sample = to3DMatrix(sample_paths, imgsize, reduction_factor)
         images_by_time[sample_index, :, :, :] = sample 
         print('Finished Processing Sample ' + str(sample_index))
-    return images_by_time
-
-def to3DMatrix(paths, imgsize=(81, 144)):
-    """
-    - len(paths) x length x width array 
-    """
-    num_images = len(paths)
-    images_by_time = np.zeros(shape=(num_images, imgsize[0], imgsize[1])) 
-    for i, path in enumerate(paths):
-        image = readImage(path)
-        images_by_time[i, :, :] = image
-        print('Finished Processing image ' + str(i))
     return images_by_time
 
 def toDurations(stopped_times_list):
@@ -69,8 +128,8 @@ def toDurations(stopped_times_list):
 
 def makeLabels(file_label, samps_per_sec=2):
     """
-    - Frames happen every .05 seconds, one sample corresponds w/ 10 frames or
-      2 samples / seconds
+    Frames happen every .05 seconds, one sample corresponds w/ 10 frames or
+    2 samples / seconds
     """
     labels_by_time = pd.read_csv(file_label, header=None)
     to_durations = toDurations(list(labels_by_time[1]))
@@ -84,14 +143,14 @@ def makeLabels(file_label, samps_per_sec=2):
 
 def makePaths(folder_root):
     """
-    - Returns the paths of all files in the folder_root 
+    Returns the paths of all files in the folder_root 
     """
     return glob.glob(os.path.join(folder_root, '*'))
 
 def makeOrderedPaths(folder_root, num_pics):
     """
-    - Returns the paths of all files in the folder_root but keeping frames in 
-      temporal order 
+    Returns the paths of all files in the folder_root but keeping frames in 
+    temporal order 
     """
     paths = [folder_root + '/image_sequence' + str(i) + '.jpeg' for i in range(1, num_pics + 1)]
     return paths
